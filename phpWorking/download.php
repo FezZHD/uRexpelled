4238ac7d7f3c71ccaa92acb3c879cec891cb2638
FezZHD <zhenya535@gmail.com>
2016-05-31 16:12:35 +0300

    updating download file

--- phpWorking/download.php
+++ phpWorking/download.php
@@ -2,19 +2,18 @@
 
 if(file_exists('../download/form.docx'));
 {
-    session_start();
-     $file= '../download/form.docx';
-      header('Content-Description: File Transfer');
-       header('Content-Type: application/msword');
+       session_start();
+       $file= '../download/form.docx';
+       header('Content-Description: File Transfer');
+       header('Content-Type: application/octet-binary');
        header('Content-Disposition: attachment; filename='.$file);
        header('Content-Transfer-Encoding: binary');
        header('Expires: 0');
        header('Cache-Control: must-revalidate');
        header('Pragma: public');
        header('Content-Length: ' . filesize($file));
-       ob_clean();
-       flush();
-       readfile($file);
+       $openedFile = fopen($file, 'rb');
+       fpassthru($openedFile);
 
       if (isset($_SESSION['isAuth']))
       {
